6f9177c68a449e2f71efc7a37a7f5e86ab6c817a
Simon Jürgensmeyer <Simon.Juergensmeyer@gmail.com>
2018-12-26 20:58:36 +0100

    Use stdin reader instead os.Args

--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"bufio"
 	"encoding/base64"
 	"encoding/hex"
 	"flag"
 	"fmt"
+	"log"
 	"os"
 	"strings"
 
@@ -20,38 +22,50 @@ func main() {
 	flag.BoolVar(&verbose, "v", false, "verbose ouput mode")
 	flag.Parse()
 
-	input := []byte(os.Args[len(os.Args)-1])
+	reader := bufio.NewReader(os.Stdin)
+	input, err := reader.ReadBytes('\n')
+	if err != nil {
+		log.Fatalln("failed to read input")
+	}
+	result, _ := decode(input)
 
+	logVerbose("result:\n")
+	fmt.Printf("%v\n", string(result))
+}
+
+func decode(input []byte) (string, int) {
 	unmarshalled := &protodec.Empty{}
+	appliedCount := 0
+
 	for {
 		applied := false
-
 		if err := proto.Unmarshal(input, unmarshalled); err == nil {
 			// TODO: remove control characters (unfortunately, they are all valid strings here)
 			input = []byte(unmarshalled.String())
 			logVerbose("applied proto decoding:\n%s\n\n", unmarshalled.String())
 			// we can't decode more on top of proto
-			break
+			appliedCount++
+			return unmarshalled.String(), appliedCount
 		}
 
 		if b, err := base64.StdEncoding.DecodeString(string(input)); err == nil {
 			input = b
 			logVerbose("applied base64 decoding:\n%v\n\n", string(b))
+			appliedCount++
 			applied = true
 		}
 
 		if b, err := hex.DecodeString(strings.ToLower(strings.TrimPrefix(string(input), "0x"))); err == nil {
 			input = b
 			logVerbose("applied hex decoding:\n%v\n\n", string(b))
+			appliedCount++
 			applied = true
 		}
 
 		if !applied {
-			break
+			return string(input), appliedCount
 		}
 	}
-	logVerbose("result:\n")
-	fmt.Printf("%v\n", string(input))
 }
 
 func logVerbose(format string, v ...interface{}) {
