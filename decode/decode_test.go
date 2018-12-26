b9e6e4c54ed5674bb4b64108c70e584fcaf11cef
Simon Jürgensmeyer <Simon.Juergensmeyer@gmail.com>
2018-12-26 23:37:18 +0100

    Added 'expect' struct

--- decode/decode_test.go
+++ decode/decode_test.go
@@ -8,62 +8,76 @@ import (
 )
 
 func TestDecode(t *testing.T) {
+	type expect struct {
+		encryption Encryption
+		output     []byte
+	}
+
 	testCases := []struct {
 		description string
-		encoded     []byte
+		given       []byte
 		decodings   int
-		applied     Encryption
-		decoded     []byte
+		expect      expect
 	}{
 		{
 			description: "no encoding",
-			encoded:     []byte("no encoding"),
-			applied:     None,
-			decoded:     []byte("no encoding"),
+			given:       []byte("no encoding"),
+			expect: expect{
+				encryption: None,
+				output:     []byte("no encoding"),
+			},
 		},
 		{
 			description: "base64",
-			encoded:     []byte(base64.StdEncoding.EncodeToString([]byte("This is a base64 test"))),
-			applied:     Base64,
-			decoded:     []byte("This is a base64 test"),
+			given:       []byte(base64.StdEncoding.EncodeToString([]byte("This is a base64 test"))),
+			expect: expect{
+				encryption: Base64,
+				output:     []byte("This is a base64 test"),
+			},
 		},
 		{
 			description: "hex",
-			encoded:     []byte(hex.EncodeToString([]byte("This is a hex test"))),
-			applied:     Hex,
-			decoded:     []byte("This is a hex test"),
+			given:       []byte(hex.EncodeToString([]byte("This is a hex test"))),
+			expect: expect{
+				encryption: Hex,
+				output:     []byte("This is a hex test"),
+			},
 		},
 		{
 			description: "0x hex",
-			encoded:     []byte("0x" + hex.EncodeToString([]byte("This is a 0xHEX test"))),
-			applied:     Hex,
-			decoded:     []byte("This is a 0xHEX test"),
+			given:       []byte("0x" + hex.EncodeToString([]byte("This is a 0xHEX test"))),
+			expect: expect{
+				encryption: Hex,
+				output:     []byte("This is a 0xHEX test"),
+			},
 		},
 		{
 			description: "proto/hex",
-			encoded:     []byte("0a03617364102a20042a200a1468747470733a2f2f657861206d706c652e636f6d1208657861206d706c653a00"),
+			given:       []byte("0a03617364102a20042a200a1468747470733a2f2f657861206d706c652e636f6d1208657861206d706c653a00"),
 			decodings:   2,
-			applied:     Proto,
-			decoded:     []byte(`1:"asd" 2:42 4:4 5:"\n\x14https://exa mple.com\x12\bexa mple" 7:"" `),
+			expect: expect{
+				encryption: Proto,
+				output:     []byte(`1:"asd" 2:42 4:4 5:"\n\x14https://exa mple.com\x12\bexa mple" 7:"" `),
+			},
 		},
 	}
 
 	for _, tt := range testCases {
 		t.Run(tt.description, func(t *testing.T) {
 			// initial decoding
-			decoded, n := Decode(tt.encoded)
+			decoded, enc := Decode(tt.given)
 
 			// apply as many decodings as specified
 			for i := 1; i < tt.decodings; i++ {
-				decoded, n = Decode(decoded)
+				decoded, enc = Decode(decoded)
 			}
 
 			// assert results
-			if n != tt.applied {
-				t.Fatalf("expeced decoding %v, got %v", tt.applied, n)
+			if enc != tt.expect.encryption {
+				t.Fatalf("expeced decoding %v, got %v", tt.expect.encryption, enc)
 			}
-			if bytes.Compare(decoded, tt.decoded) != 0 {
-				t.Fatalf("expected '%v', got '%v'", tt.decoded, decoded)
+			if bytes.Compare(decoded, tt.expect.output) != 0 {
+				t.Fatalf("expected '%v', got '%v'", tt.expect.output, decoded)
 			}
 		})
 	}
