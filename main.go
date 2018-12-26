b7ed769a94e55e5e2e4a2a90e2a4bab3869635ee
Simon Jürgensmeyer <Simon.Juergensmeyer@gmail.com>
2018-12-26 20:22:42 +0100

    Removed cobra dependency

--- main.go
+++ main.go
@@ -1,7 +1,62 @@
 package main
 
-import "github.com/sj14/multidecode/cmd"
+import (
+	"encoding/base64"
+	"encoding/hex"
+	"flag"
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/golang/protobuf/proto"
+	"github.com/sj14/multidecode/protodec"
+)
+
+var (
+	verbose bool
+)
 
 func main() {
-	cmd.Execute()
+	flag.BoolVar(&verbose, "v", false, "verbose ouput mode")
+	flag.Parse()
+
+	input := []byte(os.Args[len(os.Args)-1])
+
+	unmarshalled := &protodec.Empty{}
+	for {
+		applied := false
+
+		if err := proto.Unmarshal(input, unmarshalled); err == nil {
+			// TODO: remove control characters (unfortunately, they are all valid strings here)
+			input = []byte(unmarshalled.String())
+			logVerbose("applied proto decoding:\n%s\n\n", unmarshalled.String())
+			// we can't decode more on top of proto
+			break
+		}
+
+		if b, err := base64.StdEncoding.DecodeString(string(input)); err == nil {
+			input = b
+			logVerbose("applied base64 decoding:\n%v\n\n", string(b))
+			applied = true
+		}
+
+		if b, err := hex.DecodeString(strings.ToLower(strings.TrimPrefix(string(input), "0x"))); err == nil {
+			input = b
+			logVerbose("applied hex decoding:\n%v\n\n", string(b))
+			applied = true
+		}
+
+		if !applied {
+			break
+		}
+	}
+	logVerbose("result:\n")
+	fmt.Printf("%v\n", string(input))
+}
+
+func logVerbose(format string, v ...interface{}) {
+	if !verbose {
+		return
+	}
+	fmt.Printf(format, v...)
 }
